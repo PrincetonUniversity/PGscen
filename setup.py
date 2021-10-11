69830b185c00a14bd041be77857fd74b579d7258
Michal Grzadkowski <mgrzad@gmail.com>
2021-10-11 11:17:27 -0400

    updating package infrastructure

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 #!/bin/usr/env python
-import glob
-import sys
 import os
-
 from setuptools import setup, find_namespace_packages
+
 
 packages = find_namespace_packages(include=['pgscen.*'])
 
 setup(name='pgscen',
       version='0.1',
-      description='Power grid scenario creation platform for load and production of wind and solar',
+      description=('Power grid scenario creation platform for'
+                   'load and production of wind and solar'),
       author='Xinshuo Yang',
       author_email='xy3134@princeton.edu',
       packages=packages,
-      install_requires=['rpy2','numpy','matplotlib','pandas','scipy','jupyter','openpyxl']                    
-     )
+
+      entry_points = {
+            'console_scripts': [
+                  'pgscen-load-solar=pgscen.command_line:run_load_solar_joint',
+                  ],
+            },
+
+      install_requires=['rpy2', 'numpy', 'matplotlib', 'pandas',
+                        'scipy', 'jupyter', 'openpyxl']
+      )
+
+
+os.system("curl https://carmona.princeton.edu/SVbook/Rsafd.zip "
+          "--output Rsafd.zip")
+os.system("unzip Rsafd.zip")
+os.system('R -e "install.packages(\'Rsafd\', repos = NULL, type=\'source\')"')
