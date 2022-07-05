a96a4822d6c0ef24861f727be466170cf4dc95c6
Michal Grzadkowski <mgrzad@gmail.com>
2022-07-05 16:56:16 -0400

    harmonizing RTS command line interface with the default T7k interface

--- pgscen/rts_gmlc/command_line.py
+++ pgscen/rts_gmlc/command_line.py
@@ -2,10 +2,13 @@
 
 import argparse
 from pathlib import Path
-import pandas as pd
+import shutil
 import time
 import bz2
 import dill as pickle
+
+import numpy as np
+import pandas as pd
 
 from ..command_line import parent_parser
 from ..engine import GeminiEngine, SolarGeminiEngine
@@ -19,223 +22,218 @@ rts_parser = argparse.ArgumentParser(add_help=False, parents=[parent_parser])
 rts_parser.add_argument('rts_dir', type=str,
                         help="where RTS-GMLC repository is stored")
 
+joint_parser = argparse.ArgumentParser(parents=[rts_parser], add_help=False)
+joint_parser.add_argument('--use-all-load-history',
+                          action='store_true', dest='use_all_load_hist',
+                          help="train load models using all out-of-sample "
+                               "historical days instead of the same "
+                               "window used for solar models")
 
-#TODO: consolidate the code for these interfaces like we do for t7k?
+
 def run_rts():
-    parser = argparse.ArgumentParser(
+    args = argparse.ArgumentParser(
         'pgscen-rts', parents=[rts_parser],
         description="Create day-ahead RTS load, wind, and solar scenarios."
-        )
+        ).parse_args()
 
-    args = parser.parse_args()
-    start = ' '.join([args.start, "08:00:00"])
-
-    load_zone_actual_df, load_zone_forecast_df = load_load_data(args.rts_dir)
-    (wind_site_actual_df, wind_site_forecast_df,
-        wind_meta_df) = load_wind_data(args.rts_dir)
-    (solar_site_actual_df, solar_site_forecast_df,
-        solar_meta_df) = load_solar_data(args.rts_dir)
-
-    if args.verbose >= 2:
-        t0 = time.time()
-
-    for scenario_start_time in pd.date_range(start=start, periods=args.days,
-                                             freq='D', tz='utc'):
-        date_lbl = scenario_start_time.strftime('%Y-%m-%d')
-
-        scen_timesteps = pd.date_range(start=scenario_start_time,
-                                       periods=24, freq='H')
-
-        if args.verbose >= 1:
-            print("Creating RTS-GMLC load+wind+solar scenarios for: {}".format(
-                date_lbl))
-
-        if args.pickle:
-            out_fl = Path(args.out_dir, "scens_{}.p.gz".format(date_lbl))
-
-            if args.skip_existing and out_fl.exists():
-                continue
-
-        # for RTS we always do in-sample since we only have a year of data
-        (load_zone_actual_hists,
-            load_zone_actual_futures) = split_actuals_hist_future(
-                    load_zone_actual_df, scen_timesteps, in_sample=True)
-        (load_zone_forecast_hists,
-            load_zone_forecast_futures) = split_forecasts_hist_future(
-                    load_zone_forecast_df, scen_timesteps, in_sample=True)
-
-        (wind_site_actual_hists,
-            wind_site_actual_futures) = split_actuals_hist_future(
-                    wind_site_actual_df, scen_timesteps, in_sample=True)
-        (wind_site_forecast_hists,
-            wind_site_forecast_futures) = split_forecasts_hist_future(
-                    wind_site_forecast_df, scen_timesteps, in_sample=True)
-
-        (solar_site_actual_hists,
-            solar_site_actual_futures) = split_actuals_hist_future(
-                    solar_site_actual_df, scen_timesteps, in_sample=True)
-        (solar_site_forecast_hists,
-            solar_site_forecast_futures) = split_forecasts_hist_future(
-                    solar_site_forecast_df, scen_timesteps, in_sample=True)
-
-        load_engn = GeminiEngine(load_zone_actual_hists,
-                                 load_zone_forecast_hists,
-                                 scenario_start_time, asset_type='load')
-
-        load_engn.fit(5e-2, 5e-2)
-        load_engn.create_scenario(args.scenario_count,
-                                  load_zone_forecast_futures,
-                                  bin_width_ratio=0.1, min_sample_size=400)
-
-        if not args.pickle:
-            load_engn.write_to_csv(args.out_dir, load_zone_actual_futures,
-                                   write_forecasts=True)
-
-        wind_engn = GeminiEngine(wind_site_actual_hists,
-                                 wind_site_forecast_hists,
-                                 scenario_start_time, wind_meta_df, 'wind')
-
-        dist = wind_engn.asset_distance().values
-        wind_engn.fit(dist / (10 * dist.max()), 5e-2)
-        wind_engn.create_scenario(args.scenario_count,
-                                  wind_site_forecast_futures)
-
-        if not args.pickle:
-            wind_engn.write_to_csv(args.out_dir, wind_site_actual_futures,
-                                   write_forecasts=True)
-
-        solar_engn = SolarGeminiEngine(solar_site_actual_hists,
-                                       solar_site_forecast_hists,
-                                       scenario_start_time, solar_meta_df,
-                                       us_state='California')
-
-        solar_engn.fit_solar_model(
-            hist_start='2020-01-01', hist_end='2020-12-31')
-        solar_engn.create_solar_scenario(args.scenario_count,
-                                         solar_site_forecast_futures)
-
-        if not args.pickle:
-            solar_engn.write_to_csv(args.out_dir,
-                                    {'solar': solar_site_actual_futures},
-                                    write_forecasts=True)
-
-        else:
-            with bz2.BZ2File(out_fl, 'w') as f:
-                pickle.dump({'Load': load_engn.scenarios['load'].round(4),
-                             'Wind': wind_engn.scenarios['wind'].round(4),
-                             'Solar': solar_engn.scenarios['solar'].round(4)},
-                            f, protocol=-1)
-
-    if args.verbose >= 2:
-        if args.days == 1:
-            day_str = "day"
-        else:
-            day_str = "days"
-
-        print("Created {} each of load, wind, and solar scenarios for {} {} "
-              "in {:.1f} seconds".format(args.scenario_count,
-                                         args.days, day_str, time.time() - t0))
+    rts_runner(args.start, args.days, args.rts_dir, args.out_dir,
+               args.scenario_count, args.nearest_days, args.random_seed,
+               create_load=True, create_wind=True, create_solar=True,
+               write_csv=not args.pickle, skip_existing=args.skip_existing,
+               verbosity=args.verbose)
 
 
 def run_rts_joint():
-    parser = argparse.ArgumentParser(
-        'pgscen-rts-joint', parents=[rts_parser],
+    args = argparse.ArgumentParser(
+        'pgscen-rts-joint', parents=[joint_parser],
         description="Create day-ahead RTS wind and load-solar joint scenarios."
-        )
+        ).parse_args()
 
-    args = parser.parse_args()
-    start = ' '.join([args.start, "08:00:00"])
+    rts_runner(args.start, args.days, args.rts_dir, args.out_dir,
+               args.scenario_count, args.nearest_days, args.random_seed,
+               create_load=False, create_wind=True, create_load_solar=True,
+               write_csv=not args.pickle, skip_existing=args.skip_existing,
+               use_all_load_hist=args.use_all_load_hist,
+               verbosity=args.verbose)
 
-    load_zone_actual_df, load_zone_forecast_df = load_load_data(args.rts_dir)
-    (wind_site_actual_df, wind_site_forecast_df,
-        wind_meta_df) = load_wind_data(args.rts_dir)
-    (solar_site_actual_df, solar_site_forecast_df,
-        solar_meta_df) = load_solar_data(args.rts_dir)
 
-    if args.verbose >= 2:
+def rts_runner(start_date, ndays, rts_dir, out_dir, scen_count, nearest_days,
+               random_seed, create_load=False, create_wind=False,
+               create_solar=False, create_load_solar=False,
+               write_csv=True, skip_existing=False, use_all_load_hist=False,
+               verbosity=0):
+    start = ' '.join([start_date, "08:00:00"])
+
+    if random_seed:
+        np.random.seed(random_seed)
+
+    if create_load or create_load_solar:
+        load_zone_actual_df, load_zone_forecast_df = load_load_data(rts_dir)
+
+    if create_wind:
+        (wind_site_actual_df, wind_site_forecast_df,
+            wind_meta_df) = load_wind_data(rts_dir)
+
+    if create_solar or create_load_solar:
+        (solar_site_actual_df, solar_site_forecast_df,
+            solar_meta_df) = load_solar_data(rts_dir)
+
+    if verbosity >= 2:
         t0 = time.time()
 
-    for scenario_start_time in pd.date_range(start=start, periods=args.days,
+    for scenario_start_time in pd.date_range(start=start, periods=ndays,
                                              freq='D', tz='utc'):
         date_lbl = scenario_start_time.strftime('%Y-%m-%d')
 
         scen_timesteps = pd.date_range(start=scenario_start_time,
                                        periods=24, freq='H')
 
-        if args.verbose >= 1:
-            print("Creating RTS-GMLC wind+joint scenarios for: {}".format(
-                date_lbl))
+        if verbosity >= 1:
+            print("Creating RTS-GMLC scenarios for: {}".format(date_lbl))
 
-        if args.pickle:
-            out_fl = Path(args.out_dir, "scens_{}.p.gz".format(date_lbl))
+        if not write_csv:
+            out_fl = Path(out_dir, "scens_{}.p.gz".format(date_lbl))
 
-            if args.skip_existing and out_fl.exists():
+            if skip_existing and out_fl.exists():
                 continue
 
-        (load_zone_actual_hists,
-            load_zone_actual_futures) = split_actuals_hist_future(
-                    load_zone_actual_df, scen_timesteps, in_sample=True)
-        (load_zone_forecast_hists,
-            load_zone_forecast_futures) = split_forecasts_hist_future(
-                    load_zone_forecast_df, scen_timesteps, in_sample=True)
+            out_scens = dict()
 
-        (wind_site_actual_hists,
-            wind_site_actual_futures) = split_actuals_hist_future(
-                    wind_site_actual_df, scen_timesteps, in_sample=True)
-        (wind_site_forecast_hists,
-            wind_site_forecast_futures) = split_forecasts_hist_future(
-                    wind_site_forecast_df, scen_timesteps, in_sample=True)
+        if write_csv and skip_existing:
+            date_path = Path(out_dir, scenario_start_time.strftime('%Y%m%d'))
 
-        (solar_site_actual_hists,
-            solar_site_actual_futures) = split_actuals_hist_future(
-                    solar_site_actual_df, scen_timesteps, in_sample=True)
-        (solar_site_forecast_hists,
-            solar_site_forecast_futures) = split_forecasts_hist_future(
-                    solar_site_forecast_df, scen_timesteps, in_sample=True)
+            if ((create_load or create_load_solar)
+                    and Path(date_path, 'load').exists()):
+                shutil.rmtree(Path(date_path, 'load'))
 
-        ge = GeminiEngine(wind_site_actual_hists, wind_site_forecast_hists,
-                          scenario_start_time, wind_meta_df, 'wind')
+            if create_wind and Path(date_path, 'wind').exists():
+                shutil.rmtree(Path(date_path, 'wind'))
 
-        dist = ge.asset_distance().values
-        ge.fit(dist / (10 * dist.max()), 5e-2)
-        ge.create_scenario(args.scenario_count, wind_site_forecast_futures)
+            if ((create_solar or create_load_solar)
+                    and Path(date_path, 'solar').exists()):
+                shutil.rmtree(Path(date_path, 'solar'))
 
-        if not args.pickle:
-            ge.write_to_csv(args.out_dir, wind_site_actual_futures,
-                            write_forecasts=True)
+        # split input datasets into training and testing subsets
+        # for RTS we always do in-sample since we only have a year of data
+        if create_load or create_load_solar:
+            (load_zone_actual_hists,
+                load_zone_actual_futures) = split_actuals_hist_future(
+                        load_zone_actual_df, scen_timesteps, in_sample=True)
+            (load_zone_forecast_hists,
+                load_zone_forecast_futures) = split_forecasts_hist_future(
+                        load_zone_forecast_df, scen_timesteps, in_sample=True)
 
-        se = SolarGeminiEngine(solar_site_actual_hists,
-                               solar_site_forecast_hists,
-                               scenario_start_time, solar_meta_df,
-                               us_state='California')
+        if create_wind:
+            (wind_site_actual_hists,
+                wind_site_actual_futures) = split_actuals_hist_future(
+                        wind_site_actual_df, scen_timesteps, in_sample=True)
+            (wind_site_forecast_hists,
+                wind_site_forecast_futures) = split_forecasts_hist_future(
+                        wind_site_forecast_df, scen_timesteps, in_sample=True)
 
-        se.fit_load_solar_joint_model(
-            load_zone_actual_hists, load_zone_forecast_hists,
-            hist_start='2020-01-01', hist_end='2020-12-31'
-            )
+            wind_engn = GeminiEngine(wind_site_actual_hists,
+                                     wind_site_forecast_hists,
+                                     scenario_start_time, wind_meta_df, 'wind')
 
-        se.create_load_solar_joint_scenario(args.scenario_count,
-                                            load_zone_forecast_futures,
-                                            solar_site_forecast_futures)
+        if create_solar or create_load_solar:
+            (solar_site_actual_hists,
+                solar_site_actual_futures) = split_actuals_hist_future(
+                        solar_site_actual_df, scen_timesteps, in_sample=True)
+            (solar_site_forecast_hists,
+                solar_site_forecast_futures) = split_forecasts_hist_future(
+                        solar_site_forecast_df, scen_timesteps, in_sample=True)
 
-        if not args.pickle:
-            se.write_to_csv(args.out_dir, {'load': load_zone_actual_futures,
-                                           'solar': solar_site_actual_futures},
-                            write_forecasts=True)
+            solar_engn = SolarGeminiEngine(solar_site_actual_hists,
+                                           solar_site_forecast_hists,
+                                           scenario_start_time, solar_meta_df,
+                                           us_state='California')
 
-        else:
+        if create_load:
+            load_engn = GeminiEngine(load_zone_actual_hists,
+                                     load_zone_forecast_hists,
+                                     scenario_start_time, asset_type='load')
+
+            load_engn.fit(5e-2, 5e-2, nearest_days)
+            load_engn.create_scenario(scen_count, load_zone_forecast_futures,
+                                      bin_width_ratio=0.1, min_sample_size=400)
+
+            if write_csv:
+                load_engn.write_to_csv(out_dir, load_zone_actual_futures,
+                                       write_forecasts=True)
+            else:
+                out_scens['Load'] = load_engn.scenarios['load'].round(4)
+
+        if create_wind:
+            dist = wind_engn.asset_distance().values
+            wind_engn.fit(dist / (10 * dist.max()), 5e-2, nearest_days)
+            wind_engn.create_scenario(scen_count, wind_site_forecast_futures)
+
+            if write_csv:
+                wind_engn.write_to_csv(out_dir, wind_site_actual_futures,
+                                       write_forecasts=True)
+            else:
+                out_scens['Wind'] = wind_engn.scenarios['wind'].round(4)
+
+        if create_solar:
+            solar_engn.fit_solar_model(nearest_days=nearest_days)
+            solar_engn.create_solar_scenario(scen_count,
+                                             solar_site_forecast_futures)
+
+            if write_csv:
+                solar_engn.write_to_csv(out_dir,
+                                        {'solar': solar_site_actual_futures},
+                                        write_forecasts=True)
+            else:
+                out_scens['Solar'] = solar_engn.scenarios['solar'].round(4)
+
+        if create_load_solar:
+            solar_engn.fit_load_solar_joint_model(
+                load_zone_actual_hists, load_zone_forecast_hists,
+                nearest_days=nearest_days, use_all_load_hist=use_all_load_hist
+                )
+
+            solar_engn.create_load_solar_joint_scenario(
+                scen_count,
+                load_zone_forecast_futures, solar_site_forecast_futures
+                )
+
+            if write_csv:
+                solar_engn.write_to_csv(out_dir,
+                                        {'load': load_zone_actual_futures,
+                                         'solar': solar_site_actual_futures},
+                                        write_forecasts=True)
+            else:
+                out_scens['Load'] = solar_engn.scenarios['load'].round(4)
+                out_scens['Solar'] = solar_engn.scenarios['solar'].round(4)
+
+        if not write_csv:
             with bz2.BZ2File(out_fl, 'w') as f:
-                pickle.dump({'Wind': ge.scenarios['wind'].round(4),
-                             'Load': se.scenarios['load'].round(4),
-                             'Solar': se.scenarios['solar'].round(4)},
-                            f, protocol=-1)
+                pickle.dump(out_scens, f, protocol=-1)
 
-    if args.verbose >= 2:
-        if args.days == 1:
+    if verbosity >= 2:
+        if ndays == 1:
             day_str = "day"
         else:
             day_str = "days"
 
-        print("Created {} wind and load-solar joint scenarios for {} {} in "
-              "{:.1f} seconds".format(args.scenario_count,
-                                      args.days, day_str, time.time() - t0))
+        type_lbls = list()
+        if create_load:
+            type_lbls += ['load']
+        if create_wind:
+            type_lbls += ['wind']
+        if create_solar:
+            type_lbls += ['solar']
+        if create_load_solar:
+            type_lbls += ['joint load-solar']
+
+        if len(type_lbls) == 1:
+            type_str = type_lbls[0]
+        elif len(type_lbls) == 2:
+            type_str = ' and '.join(type_lbls)
+
+        else:
+            type_str = ', and '.join([', '.join(type_lbls[:-1]),
+                                      type_lbls[-1]])
+
+        print("Created {} {} scenarios for {} {} in {:.1f} seconds".format(
+            scen_count, type_str, ndays, day_str, time.time() - t0))
