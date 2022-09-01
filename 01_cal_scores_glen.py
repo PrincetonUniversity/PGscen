2ae2f501d8c47f7624e7a19b4b084114cdff0996
fjying <31668264+fjying@users.noreply.github.com>
2022-09-01 15:17:42 -0400

    Add file to calculate glen scores

---
+++ 01_cal_scores_glen.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import numpy as np
+import joblib
+import os
+from scipy.spatial.distance import pdist, squareform
+from itertools import combinations as combns
+import zipfile
+
+
+# scoring function
+def compute_energy_scores(scenarios: pd.DataFrame, actuals: pd.DataFrame,
+                          forecasts: pd.DataFrame) -> pd.Series:
+    scen_timesteps = scenarios.columns.unique(level=1)
+
+    fcsts = forecasts.stack().loc[scen_timesteps]
+
+    actl_devs = (actuals.stack().loc[scen_timesteps]
+                 - fcsts).reorder_levels([1, 0])
+    fcsts = fcsts.reorder_levels([1, 0])
+    scen_devs = scenarios[fcsts.index] - fcsts
+
+    actl_diff = (actl_devs - scen_devs).groupby(level=0, axis=1).apply(
+        lambda devs: ((devs ** 2).sum(axis=1) ** 0.5).mean())
+    scen_diff = scen_devs.groupby(level=0, axis=1).apply(
+        lambda devs: squareform(pdist(
+            devs.values, metric='euclidean')).mean()
+    )
+    return actl_diff - 0.5 * scen_diff
+
+
+def compute_variograms(scenarios: pd.DataFrame,
+                       actuals: pd.DataFrame, forecasts: pd.DataFrame,
+                       order: float = 1.) -> pd.Series:
+    scen_timesteps = scenarios.columns.unique(level=1)
+
+    fcsts = forecasts.stack().loc[scen_timesteps]
+
+    actl_devs = actuals.stack().loc[scen_timesteps] - fcsts
+    scen_devs = scenarios.reorder_levels([1, 0], axis=1)[fcsts.index] - fcsts
+    scores = pd.Series(0., index=scenarios.columns.unique(level=0))
+
+    for t1, t2 in combns(actl_devs.index.unique(level=0), 2):
+        actl_vars = (actl_devs.loc[t1] - actl_devs.loc[t2]).abs() ** order
+        scen_vars = ((scen_devs[t1] - scen_devs[t2]).abs() ** order).mean()
+        scores += (actl_vars - scen_vars) ** 2
+    return scores
+
+
+# process data into pgscen format
+def process_data_pgscen(assets_df):
+    forecasts = assets_df[assets_df['Type'] == 'Forecast']
+    forecasts = forecasts.drop(columns=['Type'])
+    actuals = assets_df[assets_df['Type'] == 'Actual']
+    actuals = actuals.drop(columns=['Type'])
+    scenarios = assets_df[assets_df['Type'] == 'Simulation']
+    scenarios = scenarios.drop(columns=['Type'])
+
+    forecasts = forecasts.set_index(['asset_id'])
+    forecasts.columns.name = 'time'
+    forecasts = forecasts.T
+
+    actuals = actuals.set_index(['asset_id'])
+    actuals.columns.name = 'time'
+    actuals = actuals.T
+
+    nobs = int(scenarios.shape[0])
+    nassets = int(len(scenarios['asset_id'].unique()))
+    nscenarios = int(nobs / nassets)
+    nhrs = int(scenarios.shape[1] - 1)
+
+    scenarios['scen_id'] = np.tile(np.arange(nscenarios) + 1, nassets)
+    scenarios = scenarios.set_index(['asset_id', 'scen_id'])
+    scenarios.columns.name = 'time'
+    scenarios = scenarios.stack().unstack(['asset_id', 'time'])
+    return scenarios, actuals, forecasts
+
+
+# Cal Scores
+os.chdir('/projects/PERFORM/TX_SolarWindLoad_20220427/CSV/DA')
+zipfiles_list = sorted(os.listdir())
+escores_solar = pd.DataFrame()
+escores_wind = pd.DataFrame()
+escores_load = pd.DataFrame()
+
+varios_solar = pd.DataFrame()
+varios_wind = pd.DataFrame()
+varios_load = pd.DataFrame()
+for data_date in zipfiles_list[0:2]:
+    print('---')
+    print(data_date)
+    date = data_date[-12:-4]
+    zip_ref = zipfile.ZipFile(data_date, "r")
+    for i in zipfile.Path(zip_ref).iterdir():  # wind, solar, load
+        print(i)
+        assets_df = pd.DataFrame()
+        for j in i.iterdir():
+            df = pd.read_csv(j.open())
+            df = df.drop(columns=['Index'])
+            df['asset_id'] = j.name.split('.')[0]
+            assets_df = pd.concat([assets_df, df], axis=0)
+        scenarios, actuals, forecasts = process_data_pgscen(assets_df)
+        escores = compute_energy_scores(scenarios, actuals, forecasts)
+        varios = compute_variograms(scenarios, actuals, forecasts)
+
+        if i.name == 'solar':
+            escores_solar[date] = escores
+            varios_solar[date] = varios
+
+        if i.name == 'load':
+            escores_load[date] = escores
+            varios_load[date] = varios
+
+        if i.name == 'wind':
+            escores_wind[date] = escores
+            varios_wind[date] = varios
+
+    zip_ref.close()
+
+# output scores files as new format
+escores_solar.reset_index().to_csv('escores_solar_2017_2018_glen.csv.gz', compression='gzip', index=False)
+varios_solar.reset_index().to_csv('varios_solar_2017_2018_glen.csv.gz', compression='gzip', index=False)
+
+escores_wind.reset_index().to_csv('escores_wind_2017_2018_glen.csv.gz', compression='gzip', index=False)
+varios_wind.reset_index().to_csv('varios_wind_2017_2018_glen.csv.gz', compression='gzip', index=False)
+
+escores_load.reset_index().to_csv('escores_load_2017_2018_glen.csv.gz', compression='gzip', index=False)
+varios_load.reset_index().to_csv('varios_load_2017_2018_glen.csv.gz', compression='gzip', index=False)
